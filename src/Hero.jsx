c5ee66cb74fcd5a5758fcb32842533fb39e40f88
David Inglody <inglody96@gmail.com>
2023-10-12 10:12:01 +0200

    whole project

---
+++ src/Hero.jsx
@@ -0,0 +1,12 @@
+const Hero = () => {
+  return <div className="hero-container">
+    <div className="hero-center">
+        <h1>Using MouseEnter/Over/Leave<br/> Anywhere
+      </h1>
+      <p>
+        Menu by using Strapi open-source.
+      </p>
+    </div>
+  </div>
+}
+export default Hero
---
+++ src/NavLinks.jsx
@@ -0,0 +1,15 @@
+import { useGlobalContext } from "./context"
+import sublinks from "./data"
+
+const NavLinks = () => {
+    const{setPageId}=useGlobalContext()
+  return <div className="nav-links">
+    {sublinks.map((link)=>{
+        const {pageId,page} = link
+        return <button className="nav-link" key={pageId} onMouseEnter={()=> setPageId(pageId)}>
+            {page}
+        </button>
+    })}
+  </div>
+}
+export default NavLinks
